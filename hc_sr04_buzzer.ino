2aa20e8b8f841498650150d13e4fe6ada2de484d
jnm277 <darcyjunior2011@gmail.com>
2026-03-08 17:22:10 -0300

    circuito de um sensor de movimento com buzzer

---
+++ hc_sr04_buzzer.ino
@@ -0,0 +1,42 @@
+int buzzer = 9;
+int treshold = 70;
+// pinos ligados ao sensor
+ const int trigger = 10;  // emite o sinal
+ const int echo = 11; // recebe o sinal
+
+void setup() {
+  pinMode(buzzer, OUTPUT);
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // variáveis para calcular a distância
+  long duration, cm;
+  //controlar os sinais emitidos pelo sensor
+  digitalWrite(trigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
+  // calcula a duração do impulso
+  duration = pulseIn(echo, HIGH);
+  // converte o tempo em distância
+  cm = microsecondsToCentimeters(duration);
+  if(cm<treshold){
+    digitalWrite(buzzer, HIGH);
+  }
+  else{
+    digitalWrite(buzzer, LOW);
+  }
+//demonstra a distância no serial monitor
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  delay(500);
+}
+
+long microsecondsToCentimeters(long microseconds) {
+  return microseconds / 29 / 2;
+}
